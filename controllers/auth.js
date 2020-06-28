a2614cc2e06612989cabff30ab10041d50e1390e
jadehendricks <hendricksjade@gmail.com>
2020-06-28 19:46:59 +0200

    implementing the signup functionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,27 @@
+const User = require('../models/User');
+
 exports.signup = async (req, res, next) => {
-    console.log(req.body);
-    res.json({
-        data: 'You hit the signup endpoint'
-    });
+    const { name, email, password } = req.body;
+
+    try {
+        const user = await User.findOne({ email });
+        if (user) {
+            return res.status(400).json({
+                error: 'Email already exists'
+            });
+        }
+
+        let newUser = await User.create({ name, email, password });
+
+        res.json({
+            user: newUser,
+            message: 'Signup success! Please log in.'
+        })
+
+    } catch (err) {
+        res.status(400).json({
+            error: err
+        })
+    }
+
 }
