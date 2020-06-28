81d7598fda431fe154deb5cfb9e84202a3ba56e1
jadehendricks <hendricksjade@gmail.com>
2020-06-29 00:28:56 +0200

    implementing sendgrind into the signup flow

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,12 @@
 const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+const sendGridMail = require('@sendgrid/mail');
+const dotenv = require('dotenv');
 
+dotenv.config();
+sendGridMail.setApiKey(process.env.SG_API_KEY);
+
+//Signup - email work flow
 exports.signup = async (req, res, next) => {
     const { name, email, password } = req.body;
 
@@ -7,21 +14,38 @@ exports.signup = async (req, res, next) => {
         const user = await User.findOne({ email });
         if (user) {
             return res.status(400).json({
-                error: 'Email already exists'
+                error: 'Email Address already exists'
             });
         }
 
-        let newUser = await User.create({ name, email, password });
+        const token = jwt.sign({ name, email, password }, process.env.JWT_ACCOUNT_ACTIVATION, {
+           expiresIn: '10m'
+        });
 
-        res.json({
-            user: newUser,
-            message: 'Signup success! Please log in.'
-        })
+        const emailData = {
+            from: process.env.EMAIL_FROM,
+            to: email,
+            subject: `Account activation link`,
+            html: `
+            <h1>Please user the following link to activate your account</h1>
+            <p>${process.env.CLIENT_URL}/auth/activate/${token}</p>
+            <hr/>
+            <p>This email may contain sensitive information</p>
+            <p>${process.env.CLIENT_URL}</p>
+            `
+        }
+
+        await sendGridMail.send(emailData);
+        res.status(200).json({
+            message: `Email has been sent to ${email}. Follow the instructions to activate your account`
+        });
+
+        //const newUser = await User.create({ name, email, password })
 
     } catch (err) {
-        res.status(400).json({
-            error: err
-        })
+        console.error(err);
+        return res.status(404).json({
+            error: err.message
+        });
     }
-
 }
