436e993603534ccd6598429c6fccf8b541d2520c
jadehendricks <hendricksjade@gmail.com>
2020-07-06 00:48:55 +0200

    intial auth context values added

--- client/src/context/authContext/AuthReducer.js
+++ client/src/context/authContext/AuthReducer.js
@@ -1,19 +1,27 @@
-import { 
-    SET_ACTIVITY,
-    MODAL_VISIBILITY } from '../types';
+import { LOGIN_SUCCESS, LOGIN_FAIL, REGISTER_SUCCESS, REGISTER_FAIL, LOGOUT  } from '../types';
   
 export default ( state, action ) => {
     switch (action.type) {
-        case SET_ACTIVITY:
-        return {
-            ...state,
-            activity: action.payload
-        };
-        case MODAL_VISIBILITY:
-        return {
-            ...state,
-            modalShow: action.payload
-        };
+        case LOGIN_SUCCESS:
+            return {
+                ...state,
+            };
+        case LOGIN_FAIL:
+            return {
+                ...state,
+            };
+        case REGISTER_SUCCESS:
+            return {
+                ...state,
+            };
+        case REGISTER_FAIL:
+            return {
+                ...state,
+            };
+        case LOGOUT:
+            return {
+                ...state,
+            };
         default: 
         return state;
     }
--- client/src/context/authContext/AuthState.js
+++ client/src/context/authContext/AuthState.js
@@ -1,76 +1,24 @@
 import React, {useReducer} from "react";
-import ModalContext from './ModalContext';
-import ModalReducer from './ModalReducer';
-import { 
-  MODAL_VISIBILITY,
-  SET_ACTIVITY } from '../types';
+import AuthContext from './AuthContext';
+import AuthReducer from './AuthReducer';
+import { LOGIN_SUCCESS, LOGIN_FAIL, REGISTER_SUCCESS, REGISTER_FAIL, LOGOUT  } from '../types';
 
-const ModalState = props => {
+const AuthState = props => {
 
   const initialState = {
-    activity: null,
-    modalShow: false
+    isAuthenticated: null,
+    loading: true,
+    user: null
   };
 
-  const [state, dispatch] = useReducer(ModalReducer, initialState);
-
-
-  const authSelection = (e) => {
-    const buttonClicked = e.target.getAttribute("data-auth");
-
-    dispatch({
-      type: MODAL_VISIBILITY,
-      payload: true
-    })
-
-    buttonClicked && dispatch({
-      type: SET_ACTIVITY,
-      payload: buttonClicked
-    });
-  }
-
-  const closeModal = (e) => {
-    window.addEventListener("click", (e) => {
-      if (e.target.classList.contains("overlay")) {
-        dispatch({
-          type: MODAL_VISIBILITY,
-          payload: false
-        })
-      }
-      return;
-    });
-  }
-
-  const authFailure = (e) => {
-    e.preventDefault();
-    alert("This will work in due time")
-  }
-
-  const showPassword = (e) => {
-    const eyeElement = e.currentTarget.children[0];
-    const passwordInput = e.currentTarget.previousSibling;
-    
-    if (passwordInput.getAttribute("type") === "password") {
-      passwordInput.setAttribute("type", "text");
-      eyeElement.setAttribute("xlink:href", "/static/media/sprite.12eb0e28.svg#icon-eye-blocked")
-    } else {
-      passwordInput.setAttribute("type", "password");
-      eyeElement.setAttribute("xlink:href", "/static/media/sprite.12eb0e28.svg#icon-eye")
-    }
-  }
+  const [state, dispatch] = useReducer(AuthReducer, initialState);
 
   return (
-    <ModalContext.Provider value={{
-      authSelection,
-      showPassword,
-      closeModal,
-      authFailure,
-      activity: state.activity,
-      modalShow: state.modalShow
+    <AuthContext.Provider value={{
     }}>
       {props.children}
-    </ModalContext.Provider>
+    </AuthContext.Provider>
   )
 }
 
-export default ModalState;
+export default AuthState;
